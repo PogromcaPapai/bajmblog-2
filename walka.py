d13731f106907d6cce11312d42c0e5b9c736cc22
Jakub Dakowski <jakubdakowski@gmail.com>
2019-06-08 12:40:36 +0200

    Added a working enemy representation

--- walka.py
+++ walka.py
@@ -3,9 +3,33 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 import random
 
+class jednostka():
+    ####
+    # Zadaniem klasy jest tworzenie jednostek do walki
+    ####
+    def __init__(self, imie, gracz=False):
+        self.imie=imie
+        if gracz==True:
+            self.hp='1000'
+            fota=Image.open('test.jpg')
+        else:
+            self.hp='100'
+            fota=Image.open('przeciwnik_temp.png')
+        fota=fota.resize((250,250),Image.ANTIALIAS)
+        self.obraz=ImageTk.PhotoImage(image=fota)
+
+    def zwrochp(self):
+        return self.hp
+    
+    def zwrocobraz(self):
+        return self.obraz
+    
+    def zwrocimie(self):
+        return self.imie
+
 class Walka(Frame):
     def atakprzeciwnik(self):
-        return "jeszcze nie zrobiony"
+        print('atak')
 
     def pominture(self):
         print('tura')
@@ -13,55 +37,39 @@ class Walka(Frame):
     def lecz(self):
         print('leczenie')
 
-    def __init__(self,master,eq,lvl,imie_boh,imie_przec):
+    def __init__(self,master,imie_boh,imie_przec):
+        ####
+        # Kod rozpoczynający walkę
+        ####
         Frame.__init__(self, master)
         self.master = master
-        self.hp_gracz = 1000
-        self.imie_gracz = imie_boh
-        self.imie_przec = imie_przec
-        self.hp_przec=100
+        self.gracz = jednostka(imie_boh,gracz=True)
+        self.przeciwnik = jednostka(imie_przec)
+        self.zbudujprzeciwnika()
 
-        przeciwnik = Frame(self)
-        przeciwnik_hp = Label(przeciwnik, text=self.imie_przec+'\nHP: '+str(self.hp_przec))
-        przeciwnik_hp.pack()
+    def zbudujprzeciwnika(self):
+        ####
+        # Tworzy obszar wyświetlający przeciwnika
+        ####
+        rama_przec = Frame(self)
 
-        # Obraz przeciwnika
-        przeciwnik_canvas = Canvas(przeciwnik, width = 200, height=200)
-        przeciwnik_canvas.pack()
-        przeciwnik_plik=ImageTk.PhotoImage(Image.open('przeciwnik_temp.png'))
-        przeciwnik_canvas.create_image(70,70,image=przeciwnik_plik)
+        # Informacje
+        hp = Label(rama_przec, text=self.przeciwnik.zwrocimie()+'\nHP: '+self.przeciwnik.zwrochp())
+        hp.pack()
+        
+        # Obraz
+        plotno = Label(rama_przec, image=self.przeciwnik.zwrocobraz())
+        plotno.pack()
+        rama_przec.place(relx=0.65,rely=0.01)
 
-        przeciwnik.pack(side=LEFT)
-        #Obraz bohatera
-        bohater_canvas = Canvas(self, width = 200, height=200)
-        bohater_canvas.place(x=100, y=200)
-        bohater_plik=ImageTk.PhotoImage(Image.open('bohater_temp.png'))
-        bohater_canvas.create_image(70,70,image=bohater_plik)
-
-        # Informacja o HP gracza
-        bohater_hp = Label(self, text='\nHP: '+str(self.hp_gracz))
-        bohater_hp.place(x=180,y=405)
-
-        ### Interfejs - defensywa ###
-        # Omijanie tury
-        przycisk_omin = Button(self, text="Pomiń turę")
-        przycisk_omin.config(height=2,width=13)
-        przycisk_omin.place(x= 100, y=450)
-
-        # Poddanie się
-        przycisk_poddaj = Button(self, text="Poddaj się", command=self.quit)
-        przycisk_poddaj.config(height=2,width=13)
-        przycisk_poddaj.place(x= 206, y=450)
-
-        # Leczenie
-        przycisk_lecz = Button(self, text="Ulecz się")
-        przycisk_lecz.config(height=2, width = 28)
-        przycisk_lecz.place(x=100, y=498)
-
-        ### Interfejs - ofensywa ###
+    def zbudujbohatera(self):
+        ####
+        # Tworzy obszar wyświetlający gracza
+        ####
+        print('x')
 
 
 okno = Tk()
-okno.geometry('960x720')
-Walka(okno,[1,2],1,'pies','kot').pack()
+okno.geometry('960x640')
+Walka(okno,'pies','kot').place(relwidth=1,relheight=1)
 okno.mainloop()
