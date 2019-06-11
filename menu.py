4cd750a72f06efa7edad4a68e1c81e379578d54d
Jakub Dakowski <jakubdakowski@gmail.com>
2019-06-11 21:26:30 +0200

    Preparing the program for scrapper implementation

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@
 
 from tkinter import *
 from tkinter import messagebox
-from PIL import Image, ImageTK
+from PIL import Image, ImageTk
 
 #funkcje przycisków
 
@@ -10,8 +10,14 @@ def autorzy():
     messagebox.showinfo("Twórcy tego cuda","Jakub Dakowski\nNatalia Majda\nWojciech Zięba")
 def zacznij_gierke():
     messagebox.showinfo("Rozpocznij gierkę","Za-za-zaczynajmy!")
-def zmien_zespol():
+def baza_bajm():
+    print('xD')
+def baza_przeciwnik():
     messagebox.showinfo("Zmienić zespół?","Tutaj będzie opcja zmiany przeciwnika but wait")
+    print('xD')
+def baza_both():
+    baza_bajm()
+    baza_przeciwnik()
 
 
 glowneOkno=Tk()
@@ -19,8 +25,7 @@ glowneOkno=Tk()
 pasekMenu=Menu(glowneOkno)
 pierwszeMenu=Menu(pasekMenu,tearoff=0)
 
-
-pierwszeMenu.add_command(label="Zmień przeciwnika",command=zmien_zespol)
+pierwszeMenu.add_command(label="Zaktualizuj bazę cytatów",command=baza_both)
 pierwszeMenu.add_command(label="Zamknij",command=glowneOkno.quit)
 pasekMenu.add_cascade(label="Opcje",menu=pierwszeMenu)
 
@@ -28,15 +33,25 @@ pomocMenu=Menu(pasekMenu,tearoff=0)
 pomocMenu.add_command(label="O Autorach",command=autorzy)
 pasekMenu.add_cascade(label="Autorzy",menu=pomocMenu)
 
-przycisk_rozpoczecie=Button(glowneOkno,text="Rozpocznij grę!",command=zacznij_gierke)
-przycisk_rozpoczecie.place(x=60,y=70)
-
-glowneOkno.config(menu=pasekMenu)
-glowneOkno.mainloop()
-
 #daje logo i kocham śledzie
 
-logo_canvas= Canvas(glowneOkno,width=1000,height=200)
+logo_canvas= Canvas(glowneOkno,width=500,height=150)
 logo_canvas.pack()
 logo_pliczek=ImageTk.PhotoImage(Image.open('logo.gif'))
-logo_canvas.create_image(479,100,image=logo_plik)
+logo_canvas.create_image(250,60,image=logo_pliczek)
+
+przycisk_rozpoczecie=Button(glowneOkno,text="Rozpocznij grę!",command=zacznij_gierke)
+przycisk_rozpoczecie.pack()
+
+zespol = StringVar(glowneOkno, value="https://www.tekstowo.pl/piosenki_artysty,golec_uorkiestra")
+zmiana_zespolu_tekst=Label(glowneOkno, text="\nPodaj adres do strony tekstowo.pl wybranego zespołu:")
+zmiana_zespolu_tekst.pack()
+zmiana_zespolu=Entry(glowneOkno, textvariable=zespol, width=40)
+zmiana_zespolu.pack()
+zmiana_zespolu_przycisk=Button(glowneOkno, text="Zmień przeciwnika!", command=baza_przeciwnik)
+zmiana_zespolu_przycisk.pack()
+
+glowneOkno.config(menu=pasekMenu)
+glowneOkno.geometry('960x640')
+
+glowneOkno.mainloop()
